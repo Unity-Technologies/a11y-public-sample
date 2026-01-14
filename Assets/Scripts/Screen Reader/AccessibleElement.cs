11b96a5a22193d01dfcd7c40e935cb5353c6930d
Bianca Marina Stana <bianca.stana@unity3d.com>
2026-01-14 11:18:33 -0500

    Updated the project to 6000.3.4f1

--- Assets/Scripts/Screen Reader/AccessibleElement.cs
+++ Assets/Scripts/Screen Reader/AccessibleElement.cs
@@ -128,8 +128,8 @@ namespace Unity.Samples.ScreenReader
                 return;
             }
 
-            node.selected -= InvokeSelected;
-            node.selected += InvokeSelected;
+            node.invoked -= InvokeSelected;
+            node.invoked += InvokeSelected;
         }
 
         void ConnectNodeToDismissed()
@@ -154,7 +154,7 @@ namespace Unity.Samples.ScreenReader
             }
 
             // Disconnect even if selected is implemented when the node is unset.
-            node.selected -= InvokeSelected;
+            node.invoked -= InvokeSelected;
         }
 
         void DisconnectNodeFromDismissed()
